eb29ba3556b692d0fc14f8f91317231a4c99dadf
chenlinwangwang <422828518@qq.com>
2016-09-14 00:14:45 +0800

    first commit

---
+++ app/src/main/java/clw/com/fragmenttabhost/fra/Home1Fra.java
@@ -0,0 +1,47 @@
+package clw.com.fragmenttabhost.fra;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+import clw.com.fragmenttabhost.R;
+
+
+/**
+ * home1
+ * 
+ * @author andye
+ * 
+ */
+public class Home1Fra extends Fragment implements OnClickListener {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.index_1, null);
+		init(v);
+
+		return v;
+	}
+
+	private void init(View v) {
+
+		// headerIV = (ImageView) v.findViewById(R.id.person2_header_iv);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		// case R.id.person2_shopping: // ��ť����
+		// break;
+
+		default:
+			break;
+		}
+	}
+
+}
---
+++ app/src/main/java/clw/com/fragmenttabhost/fra/Home2Fra.java
@@ -0,0 +1,32 @@
+package clw.com.fragmenttabhost.fra;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+import clw.com.fragmenttabhost.R;
+
+/**
+ * home2
+ * @author andye
+ *
+ */
+public class Home2Fra extends Fragment implements OnClickListener {
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.index_2, null);
+
+		return v;
+	}
+
+	@Override
+	public void onClick(View v) {
+	}
+
+}
